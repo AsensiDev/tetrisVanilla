0fdb8ff96d0693ad822f056ea4d3d1306be1675c
Pablo Asensi <142694239+AsensiDev@users.noreply.github.com>
2024-08-31 15:11:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css'
+
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+//2. game loop
+function update() {
+  draw()
+  window.requestAnimationFrame(update)
+} 
+
+function draw() {
+  context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+update()
